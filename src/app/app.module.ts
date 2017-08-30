b2479a022afbfca9ab5a4f2eceaa601c984350a0
Boris <bili1@student.monash.edu>
2017-08-30 18:18:23 +1000

    Nvd3 graphs project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BarChartComponent } from './bar-chart/bar-chart.component';
+
+import 'd3';
+import 'nvd3';
+import {NvD3Module} from 'ng2-nvd3';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarChartComponent
   ],
   imports: [
-    BrowserModule
+    [BrowserModule, NvD3Module]
   ],
   providers: [],
   bootstrap: [AppComponent]
